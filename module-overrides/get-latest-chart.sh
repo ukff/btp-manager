9fb9a1a7da118fe56b8a3ab1f73b50394f03c779
LJ <lukasz.jezak@sap.com>
2023-01-24 16:17:05 +0100

    update workflow

--- module-overrides/get-latest-chart.sh
+++ module-overrides/get-latest-chart.sh
@@ -5,8 +5,8 @@ latest=$(curl \
         https://api.github.com/repos/SAP/sap-btp-service-operator/releases/latest | jq -r '.tag_name') 
 curl -L https://github.com/SAP/sap-btp-service-operator/releases/download/$latest/sap-btp-operator-$latest.tgz > charts.tgz
 tar zxvf charts.tgz 
-mv sap-btp-operator/templates/* module-chart/templates
-mv sap-btp-operator/* module-chart
+mv -v sap-btp-operator/* module-chart
+mv -v sap-btp-operator/templates/* module-chart/templates
 rm -r sap-btp-operator
 rm charts.tgz
 echo $latest
