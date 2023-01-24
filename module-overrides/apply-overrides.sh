1fdb95bc9a91424e94ec28cb040130567be65e2b
LJ <lukasz.jezak@sap.com>
2023-01-24 14:51:01 +0100

    update workflow

---
+++ module-overrides/apply-overrides.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo $1
+curl -J -O -L https://raw.githubusercontent.com/ukff/btp-manager/charts-handling/module-overrides/public-overrides.yaml
+helm template $1 . --output-dir rendered --values public-overrides.yaml
+mv rendered/sap-btp-operator/templates/ ../module-resources
+rm -r rendered/
