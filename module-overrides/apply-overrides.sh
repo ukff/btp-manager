89796dd84f04247fe853fa22da8768d1944be288
LJ <lukasz.jezak@sap.com>
2023-01-25 10:46:53 +0100

    diff

--- module-overrides/apply-overrides.sh
+++ module-overrides/apply-overrides.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+ls -la
 helm template $1 module-chart --output-dir rendered --values public-overrides.yaml
 mv rendered/sap-btp-operator/templates/ ../module-resources
 rm -r rendered/
