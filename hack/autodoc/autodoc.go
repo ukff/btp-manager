f3b800ba62c800c102c10426cd80a10f2a39da8d
LJ <lukasz.jezak@sap.com>
2023-03-24 15:13:56 +0100

    wip - temp commit msg

--- hack/autodoc/autodoc.go
+++ hack/autodoc/autodoc.go
@@ -9,6 +9,10 @@ import (
 	"strings"
 )
 
+const (
+	spaceMargin int = 10
+)
+
 type tableRow struct {
 	crState         string
 	conditionType   string
@@ -41,26 +45,27 @@ func renderTable(rows []tableRow) string {
 		}
 	}
 
-	var res string
-	res += fmt.Sprintf("| %s | %s | %s | %s | %s | %s |", appendEmptySpace(10, "No.", " "), appendEmptySpace(10, "CR state", " "), appendEmptySpace(10, "Condition type", " "), appendEmptySpace(10, "Condition status", " "), appendEmptySpace(longestConditionReasons, "Condition reason", " "), appendEmptySpace(longestRemark, "Remark", " "))
-	res += "\n"
-	res += fmt.Sprintf("| %s | %s | %s | %s | %s | %s |", appendEmptySpace(10, "", "-"), appendEmptySpace(10, "", "-"), appendEmptySpace(10, "", "-"), appendEmptySpace(10, "", "-"), appendEmptySpace(longestConditionReasons, "", "-"), appendEmptySpace(longestRemark, "", "-"))
-	res += "\n"
-	no := 1
+	var mdTable string
+	mdTable += fmt.Sprintf("| %s | %s | %s | %s | %s | %s |", appendEmptySpace(10, "No.", " "), appendEmptySpace(10, "CR state", " "), appendEmptySpace(10, "Condition type", " "), appendEmptySpace(10, "Condition status", " "), appendEmptySpace(longestConditionReasons, "Condition reason", " "), appendEmptySpace(longestRemark, "Remark", " "))
+	mdTable += "\n"
+	mdTable += fmt.Sprintf("| %s | %s | %s | %s | %s | %s |", appendEmptySpace(10, "", "-"), appendEmptySpace(10, "", "-"), appendEmptySpace(10, "", "-"), appendEmptySpace(10, "", "-"), appendEmptySpace(longestConditionReasons, "", "-"), appendEmptySpace(longestRemark, "", "-"))
+	mdTable += "\n"
 
+	lineNumber := 1
 	for _, row := range rows {
-		res += fmt.Sprintf("| %s | %s | %s | %s | %s | %s |", appendEmptySpace(10, strconv.Itoa(no), " "), appendEmptySpace(10, row.crState, " "), appendEmptySpace(10, row.conditionType, " "), appendEmptySpace(10, strconv.FormatBool(row.conditionStatus), " "), appendEmptySpace(longestConditionReasons, row.conditionReason, " "), appendEmptySpace(longestRemark, row.remark, " "))
-		res += "\n"
-		no++
+		mdTable += fmt.Sprintf("| %s | %s | %s | %s | %s | %s |", appendEmptySpace(10, strconv.Itoa(lineNumber), " "), appendEmptySpace(10, row.crState, " "), appendEmptySpace(10, row.conditionType, " "), appendEmptySpace(10, strconv.FormatBool(row.conditionStatus), " "), appendEmptySpace(longestConditionReasons, row.conditionReason, " "), appendEmptySpace(longestRemark, row.remark, " "))
+		mdTable += "\n"
+		lineNumber++
 	}
-	return res
+
+	return mdTable
 }
 
 func appendEmptySpace(x int, s string, c string) string {
 	x = x - len(s)
 	e := ""
 	e += s
-	for i := 0; i < x+10; i++ {
+	for i := 0; i < x+spaceMargin; i++ {
 		e += c
 	}
 	return e
@@ -73,22 +78,20 @@ func sortByConditionReasons(x []tableRow) {
 }
 
 func main() {
-
 	cmd := exec.Command("/bin/sh", "table.sh")
-	var outb, errb bytes.Buffer
-	cmd.Stdout = &outb
-	cmd.Stderr = &errb
-	err := cmd.Run()
-	if err != nil {
-		fmt.Println(errb.String())
+	var cmdOut, cmdErr bytes.Buffer
+	cmd.Stdout = &cmdOut
+	cmd.Stderr = &cmdErr
+	if err := cmd.Run(); err != nil {
+		fmt.Println(cmdErr.String())
 		panic(err)
 	}
-	p := strings.Split(outb.String(), "====")
+
+	inputData := strings.Split(cmdOut.String(), "====")
 	holder := holder{}
-	metadata := strings.Split(p[1], "!")
-	for _, v := range metadata {
-		fmt.Println(v)
-		holder.lineToRow(v)
+	reasonMetadata := strings.Split(inputData[1], "!")
+	for _, dataLine := range reasonMetadata {
+		holder.lineToRow(dataLine)
 	}
 
 	sortByConditionReasons(holder.ready)
@@ -96,13 +99,13 @@ func main() {
 	sortByConditionReasons(holder.deleting)
 	sortByConditionReasons(holder.error)
 
-	ctr := make([]tableRow, 0)
-	ctr = append(ctr, holder.ready...)
-	ctr = append(ctr, holder.proccessing...)
-	ctr = append(ctr, holder.deleting...)
-	ctr = append(ctr, holder.error...)
+	allTableRows := make([]tableRow, 0)
+	allTableRows = append(allTableRows, holder.ready...)
+	allTableRows = append(allTableRows, holder.proccessing...)
+	allTableRows = append(allTableRows, holder.deleting...)
+	allTableRows = append(allTableRows, holder.error...)
 
-	expectedTable := renderTable(ctr)
+	expectedTable := renderTable(allTableRows)
 	fmt.Println(expectedTable)
 }
 
@@ -113,31 +116,21 @@ func (h *holder) lineToRow(line string) {
 	if len(words) > 0 {
 		reason := words[0]
 		conditionType := "Ready"
-		crState := ""
 		if len(parts) >= 2 {
 			comments := strings.Split(parts[1], ";")
-
 			if len(comments) > 0 {
 				state = comments[0]
 				remark = comments[1]
-			} else {
-				state = "tba"
-				remark = "Tba"
 			}
-		} else {
-			state = "tba"
-			remark = "Tba"
 		}
 
-		conditionStats := calculateConditionStatus(crState, conditionType)
-		fmt.Println(conditionStats)
+		conditionStats := state == "Ready" && conditionType == "Ready"
 		cleanString(&state)
-		cleanString(&crState)
 		cleanString(&conditionType)
 		cleanString(&remark)
 		cleanString(&reason)
 
-		tr := &tableRow{
+		row := &tableRow{
 			crState:         state,
 			conditionType:   conditionType,
 			conditionStatus: conditionStats,
@@ -147,19 +140,15 @@ func (h *holder) lineToRow(line string) {
 
 		switch state {
 		case "Ready":
-			h.ready = append(h.ready, *tr)
+			h.ready = append(h.ready, *row)
 		case "Processing":
-			h.proccessing = append(h.proccessing, *tr)
+			h.proccessing = append(h.proccessing, *row)
 		case "Error":
-			h.error = append(h.error, *tr)
+			h.error = append(h.error, *row)
 		case "Deleting":
-			h.deleting = append(h.deleting, *tr)
+			h.deleting = append(h.deleting, *row)
 		}
 	}
-}
-
-func calculateConditionStatus(crState, conditionType string) bool {
-	return crState == "Ready" && conditionType == "Ready"
 }
 
 func cleanString(s *string) {
