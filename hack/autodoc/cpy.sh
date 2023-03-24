0e14ae4c19c0e4bc1992104d079c71e8d51ae747
LJ <lukasz.jezak@sap.com>
2023-03-24 12:29:19 +0100

    Remove

--- hack/autodoc/cpy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-
-operationsmd=$(cat ../../docs/operations.md)
-table=$( echo "$operationsmd" | sed -n '/\/\/ gophers_table_start/,/\/\/ gophers_table_end/p')
-echo "$table"
