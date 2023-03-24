57f2403f2175a109244e33400e47e639a91aca42
LJ <lukasz.jezak@sap.com>
2023-03-24 14:43:55 +0100

    wip - temp commit msg

--- hack/autodoc/autodoc.go
+++ hack/autodoc/autodoc.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os/exec"
 	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -24,18 +25,45 @@ type holder struct {
 }
 
 func renderTable(rows []tableRow) string {
+	longestConditionReasons := 0
+	for _, row := range rows {
+		l := len(row.conditionReason)
+		if l > longestConditionReasons {
+			longestConditionReasons = l
+		}
+	}
+
+	longestRemark := 0
+	for _, row := range rows {
+		l := len(row.remark)
+		if l > longestRemark {
+			longestRemark = l
+		}
+	}
+
 	var res string
-	res += "| No. | CR state    | Condition type | Condition status  | Condition reason | Remark "
+	res += fmt.Sprintf("| %s | %s | %s | %s | %s | %s |", appendEmptySpace(10, "No.", " "), appendEmptySpace(10, "CR state", " "), appendEmptySpace(10, "Condition type", " "), appendEmptySpace(10, "Condition status", " "), appendEmptySpace(longestConditionReasons, "Condition reason", " "), appendEmptySpace(longestRemark, "Remark", " "))
 	res += "\n"
-	res += "|-|-|-|-|-|-|"
+	res += fmt.Sprintf("| %s | %s | %s | %s | %s | %s |", appendEmptySpace(10, "", "-"), appendEmptySpace(10, "", "-"), appendEmptySpace(10, "", "-"), appendEmptySpace(10, "", "-"), appendEmptySpace(longestConditionReasons, "", "-"), appendEmptySpace(longestRemark, "", "-"))
 	res += "\n"
 	no := 1
+
 	for _, row := range rows {
-		res += fmt.Sprintf("|%d| %s| %s| %t| %s| %s", no, row.crState, row.conditionType, row.conditionStatus, row.conditionReason, row.remark)
+		res += fmt.Sprintf("| %s | %s | %s | %s | %s | %s |", appendEmptySpace(10, strconv.Itoa(no), " "), appendEmptySpace(10, row.crState, " "), appendEmptySpace(10, row.conditionType, " "), appendEmptySpace(10, strconv.FormatBool(row.conditionStatus), " "), appendEmptySpace(longestConditionReasons, row.conditionReason, " "), appendEmptySpace(longestRemark, row.remark, " "))
 		res += "\n"
 		no++
 	}
 	return res
+}
+
+func appendEmptySpace(x int, s string, c string) string {
+	x = x - len(s)
+	e := ""
+	e += s
+	for i := 0; i < x+10; i++ {
+		e += c
+	}
+	return e
 }
 
 func ss(x []tableRow) {
