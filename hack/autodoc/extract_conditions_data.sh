7b356285ce87619a14d61104191a1a5f71bc27cc
LJ <lukasz.jezak@sap.com>
2023-03-28 14:30:23 +0200

    wip - temp commit msg

--- hack/autodoc/extract_conditions_data.sh
+++ hack/autodoc/extract_conditions_data.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-conditions=$(cat ../../controllers/conditions.go)
-constReasonsInGo=$( echo "$conditions" | sed -n '/\/\/ gophers_reasons_section_start/,/\/\/ gophers_reasons_section_end/p')
-constReasonsClean=$(echo "$constReasonsInGo" | awk 'NR > 1 {print $1}' RS='(' FS=')' )
-echo "$constReasonsClean"
+
+awk '/gophers_reasons_section_start/,/gophers_reasons_section_end/' < ../../controllers/conditions.go | grep '='
 printf "===="
-metadataReasonsInGo=$( echo "$conditions" | sed -n '/\/\/ gophers_metadata_section_start/,/\/\/ gophers_metadata_section_end/p')
-metadataReasonsClean=$( echo "$metadataReasonsInGo" | awk 'NR > 1 {print $1}' RS='{' FS='}')
-echo "$metadataReasonsClean"
+
+awk '/gophers_metadata_section_start/,/gophers_metadata_section_end/' < ../../controllers/conditions.go | grep ':'
 printf "===="
-printf "\n"
-operations=$(cat ../../docs/operations.md)
-table=$( echo "$operations" | sed -n '/\/\/ gophers_table_start/,/\/\/ gophers_table_end/p')
-echo "$table"
+
+awk '/table_start/,/table_end/' < ../../docs/operations.md | grep "|"
